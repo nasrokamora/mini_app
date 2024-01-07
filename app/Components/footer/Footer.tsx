5eaca2b2c2eea0caebdcdd9a1328caf4ed51ae56
Nas@Dev <116016564+nasrokamora@users.noreply.github.com>
2024-01-07 11:36:10 +0000

    Update Footer.tsx

--- app/Components/footer/Footer.tsx
+++ app/Components/footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer(){
     return(
         <div className=" h-10 border-t border-t-white/30
         footer text-white flex justify-center items-center">
-            <p> <Link className=" text-[#8b0000] font-bold hover:animate-pulse hover:text-yellow-500 hover:duration-500" href={'https://github.com/nasdev'}>Nas@Dev</Link> Copyright © 2024. All Rights Reserved.</p>
+            <p> <Link className=" text-[#8b0000] font-bold hover:animate-pulse hover:text-yellow-500 hover:duration-500" href={'https://github.com/nasrokamora'}>Nas@Dev</Link> Copyright © 2024. All Rights Reserved.</p>
         </div>
     )
 }
